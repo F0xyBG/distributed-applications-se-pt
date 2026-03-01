3d5bc4904d63b01b614c7333909fc32d65c1f886
m.yunuz <m.yunuz@adcash.com>
2026-03-01 20:04:02 +0200

    fixes & improvements

--- course-work/Implementations/API/src/controllers/userController.js
+++ course-work/Implementations/API/src/controllers/userController.js
@@ -1,10 +1,15 @@
 import User from '../models/User.js';
+import { ACCESS_TOKEN_NAME, NODE_ENV } from '../config/constants.js';
 
 export const getUser = async (req, res) => {
   const { id } = req.user;
 
   try {
     const userData = await User.findById(id);
+
+    if (!userData) {
+      return res.sendStatus(404);
+    }
 
     res.status(200).json(userData);
   } catch (err) {
@@ -17,7 +22,11 @@ export const updateUser = async (req, res) => {
   const { id } = req.user;
 
   try {
-    await User.updateById(id, req.body);
+    const isUpdated = await User.updateById(id, req.body);
+
+    if (!isUpdated) {
+      return res.sendStatus(400);
+    }
 
     res.sendStatus(200);
   } catch (err) {
@@ -30,7 +39,17 @@ export const deleteUser = async (req, res) => {
   const { id } = req.user;
 
   try {
-    await User.deleteById(id);
+    const isDeleted = await User.deleteById(id);
+
+    if (!isDeleted) {
+      return res.sendStatus(400);
+    }
+
+    res.clearCookie(ACCESS_TOKEN_NAME, {
+      httpOnly: true,
+      secure: NODE_ENV === 'production',
+      sameSite: 'strict',
+    });
 
     res.sendStatus(200);
   } catch (err) {
